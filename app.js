02d7f4898c1d441bac5196fc9e9bf51a5568acd4
9mz1 <muzyyyyyyy@gmail.com>
2025-03-07 16:40:22 +0400

    Added the function to display books

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 const addBtn = document.querySelector('#add-btn');
+const submitBtn = document.querySelector("#submit-button")
 const bookForm = document.querySelector('#book-form');
+const booksContainer = document.querySelector('#books-container');
 
 const myLibrary = [];
 
@@ -23,10 +25,47 @@ function addBookToLibrary() {
     const newBook = new Book(title, author, pages, status);
     newBook.getinfo();
     myLibrary.push(newBook);
+    console.clear();
+    console.table(myLibrary);
+}
+
+function displayBook() {
+    // booksContainer.innerHTML = '';   
+    for (i = 0; i < myLibrary.length; i++) {
+        const card = document.createElement('div')
+        card.classList.add('card');
+
+        const bookTitle = document.createElement('h2');
+        bookTitle.classList.add('book-title');
+        bookTitle.textContent = myLibrary[i].title;
+
+        const bookAuthor = document.createElement('h3');
+        bookAuthor.classList.add('book-author');
+        bookAuthor.textContent = myLibrary[i].author;
+
+        const bookPages = document.createElement('p');
+        bookPages.classList.add('book-pages');
+        bookPages.textContent = myLibrary[i].pages;
+
+        const bookStatus = document.createElement('p')
+        bookStatus.classList.add('book-status');
+        bookStatus.textContent = myLibrary[i].status;
+
+        card.appendChild(bookTitle);
+        card.appendChild(bookAuthor);
+        card.appendChild(bookPages);
+        card.appendChild(bookStatus)
+        booksContainer.appendChild(card);
+    }
 }
 
 addBtn.addEventListener('click', () => {
     bookForm.style.display = 'block';
+});
+
+submitBtn.addEventListener('click', () => {
+    event.preventDefault();
     addBookToLibrary();
-    console.table(myLibrary);
-})
+    displayBook();
+    bookForm.reset();
+});
